cad8dd047e71dea0441908fb7953d588e3cdbeab
anaveen <aryan_naveen@college.harvard.com>
2024-10-01 14:57:56 -0400

    manager based environment

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/terminations.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/terminations.py
@@ -72,6 +72,19 @@ def root_height_below_minimum(
     return asset.data.root_pos_w[:, 2] < minimum_height
 
 
+def root_height_above_maximum(
+    env: ManagerBasedRLEnv, maximum_height: float, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot")
+) -> torch.Tensor:
+    """Terminate when the asset's root height is below the minimum height.
+
+    Note:
+        This is currently only supported for flat terrains, i.e. the minimum height is in the world frame.
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: RigidObject = env.scene[asset_cfg.name]
+    return asset.data.root_pos_w[:, 2] > maximum_height
+
+
 """
 Joint terminations.
 """
---
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/quadcopter/mdp/rewards.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from omni.isaac.lab.assets import Articulation
+from omni.isaac.lab.managers import SceneEntityCfg
+from omni.isaac.lab.utils.math import wrap_to_pi
+
+if TYPE_CHECKING:
+    from omni.isaac.lab.envs import ManagerBasedRLEnv
+
+
+def lin_vel_l2(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Penalize significant linear velocity."""
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+    return torch.sum(torch.square(asset.data.root_lin_vel_b), dim=1)
+
+def ang_vel_l2(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Penalize significant angular velocity."""
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+    return torch.sum(torch.square(asset.data.root_ang_vel_b), dim=1)
+
+def distance_to_goal_l2(env: ManagerBasedRLEnv, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+    desired_pos_w = env.command_manager.get_command("desired_drone_pose")
+    distance_to_goal = torch.linalg.norm(desired_pos_w - asset.data.root_pos_w, dim=1)
+    return 1 - torch.tanh(distance_to_goal / 0.8)
+
