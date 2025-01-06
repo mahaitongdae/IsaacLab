10c912473a397d01a7161bedf58c7eb042e70df0
naliseas-workstation <cong@gmail.com>
2025-01-06 17:23:56 -0500

    analysis

--- source/standalone/workflows/skrl_ctrl/analysis.py
+++ source/standalone/workflows/skrl_ctrl/analysis.py
@@ -3,15 +3,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import os
+from tqdm import tqdm
+
 
 parser = argparse.ArgumentParser(description="Run the analysis script with customizable parameters.")
 # Add arguments
-parser.add_argument("--experiment", type=str, default="OOD", choices=["legeval", "legood", "OOD", "legtrain"], help="Specify the task name (default: OOD).")
+parser.add_argument("--experiment", type=str, default="legeval", choices=["legeval", "legood", "OOD", "legtrain"], help="Specify the experiment name (default: OOD).")
+parser.add_argument("--num_experiments", type=int, default=1, help="Specify the number of experiments")
+
 args = parser.parse_args()
 
 output_dir = f"runs/experiments/{args.experiment}"
 
 agents = [entry.name for entry in os.scandir(output_dir) if entry.is_dir()]
 
+
+figsize_per_plot = [8, 8]
+N = args.num_experiments
+# Calculate the grid size
+cols = int(np.ceil(np.sqrt(N)))
+rows = int(np.ceil(N / cols))
+
+# Determine the figure size based on the grid and figsize_per_plot
+figsize = (figsize_per_plot[0] * cols, figsize_per_plot[1] * rows)
+fig, axes = plt.subplots(rows, cols, figsize=figsize)
+
+# Flatten the axes for easy iteration (handles both 1D and 2D cases)
+axes = axes.flatten() if N > 1 else [axes]
+
+lines = []
+labels = [] 
 for agent in agents:
-    print(agent)
+    agent_type = agent.split('-')[0]
+    agent_results = torch.load(f"{output_dir}/{agent}/results.pth")
+    MSE = []
+    for trial in tqdm(range(1, N + 1)):
+        gt = agent_results[trial]['trajectory'][0].cpu().numpy()
+        time_alive = agent_results[trial]['time_alive'][0]
+        pose = agent_results[trial]['pose'][0].cpu().numpy()
+        axes[trial - 1].plot(gt[:, 0], gt[:, 1], color='black', linewidth=2)
+        line, = axes[trial - 1].plot(pose[:time_alive, 0], pose[:time_alive, 1], label=agent_type)
+        axes[trial - 1].grid(True)
+        if agent_results[trial]['crashed']:
+            axes[trial - 1].plot(pose[time_alive, 0], pose[time_alive, 1], marker='x', color='red', linewidth=3)
+            for spine in axes[trial - 1].spines.values():
+                spine.set_edgecolor('red')  # Set the box color
+                spine.set_linewidth(4)
+            axes[trial - 1].set_facecolor((1, 0, 0, 0.1))
+        else:
+            MSE.append(agent_results[trial]['MSE'].cpu().numpy())
+        
+        if trial == 1:    
+            lines.append(line)
+            labels.append(agent_type)
+    MSE = np.array(MSE)
+    print(f"{agent_type}_MSE: {MSE.mean(axis=0)}")
+    
+for i in range(N, len(axes)):
+    axes[i].axis("off")
+
+fig.legend(
+    lines, labels,
+    loc='lower right',  # Position at the bottom right of the figure
+    fontsize=50,
+    bbox_to_anchor=(0.95, 0.05),  # Adjust the position of the legend
+    bbox_transform=fig.transFigure,
+    frameon=True  # Add a border around the legend
+)
+plt.tight_layout()
+plt.savefig(f"{output_dir}/plot.png")
